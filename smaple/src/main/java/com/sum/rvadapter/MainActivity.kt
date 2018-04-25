214b9909b48bd569c8eb1871a45aba1adcfc94b8
Sum-sdl <1079569023@qq.com>
2018-04-25 11:01:25 +0800

    update demo

--- smaple/src/main/java/com/sum/rvadapter/MainActivity.kt
+++ smaple/src/main/java/com/sum/rvadapter/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.sum.rvadapter
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 }
